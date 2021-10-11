d4ed1114817abd0844186f6d253986d4bf4a6408
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-10-11 21:28:20 +0500

    changes

--- server/index.js
+++ server/index.js
@@ -1,34 +1,50 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("sequelize-practice", "root", "amansultan", {
-    dialect: "mysql"
-})
+const { DataTypes } = Sequelize;
+const sequelize = new Sequelize(
+    "sequelize-practice",
+    "root",
+    "amansultan", {
+        dialect: "mysql"
+    })
 
 const User = sequelize.define("users", {
     name: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     },
     password: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     },
     age: {
-        type: Sequelize.DataTypes.INTEGER,
+        type: DataTypes.INTEGER,
         defaultValue: 21
     },
     isActive: {
-        type: Sequelize.DataTypes.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         defaultValue: false
     }
 },
     { freezeTableName: true }
 )
 
-User.sync({ alter: true }).then((data) => { console.log("Model Synced With Tables") })
-    .catch((e) => { console.log("Model not Synced With Tables", e) })
+User.sequelize.sync({ force: true, alter: true })
+// .then(() => {
+//     // return User.create({
+//     //     name: "Aman Sultan baig",
+//     //     password: "123456",
+//     //     age: 22,
+//     //     isActive: false
+//     // });
+// }).then((data) => {
+//     console.log(data)
+// }).catch(e => {
+//     console.log("sync error", e)
+// })
 
-sequelize.authenticate().then(() => {
-    console.log("Connection Established, App is Running")
-}).catch(e => {
-    console.log("Db Connection error " + e)
-})
+
+// sequelize.authenticate().then(() => {
+//     console.log("Connection Established, App is Running")
+// }).catch(e => {
+//     console.log("Db Connection error " + e)
+// })
