249191f451491e3b795d33dfdaff4ed780e282cc
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-10-12 23:31:58 +0500

    user model defiened

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -1,4 +1,5 @@
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.USERNAME,
@@ -12,4 +13,16 @@ sequelize.authenticate().then(() => {
     console.log("DB Connection Established!")
 }).catch(e => {
     console.log("DB Connection Error! " + e)
+})
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("../models/UserModel")(sequelize, DataTypes)
+
+db.sequelize.sync({ force: true, alter: true }).then(() => {
+    console.log("Tables are synchronized")
+}).catch(e => {
+    console.log("Error while synchronizing " + e)
 })
